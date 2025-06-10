61c615582413fa6c7d33299842ba354cf64fa14c
prthivy <prithviisinamood@gmail.com>
2025-06-10 13:52:25 +0530

    logic practice

---
+++ ch-02/output-patterns/half-square.js
@@ -0,0 +1,12 @@
+//console.log("#");
+
+// for (let i = 0; i <= 4; i++) {
+//     console.log("#");
+// }
+
+// for (let row = 1; row <= 5; row++) {
+//     for (let i = 0; i <= 4; i++) {
+//         console.log("#");
+//     } console.log(""); 
+// }
+
