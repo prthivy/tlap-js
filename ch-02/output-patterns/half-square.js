7c01203d4c9b628de332aef8f831a6b3aa986ade
prthivy <prithviisinamood@gmail.com>
2025-06-16 12:16:38 +0530

    logic practice

--- ch-02/output-patterns/half-square.js
+++ ch-02/output-patterns/half-square.js
@@ -1,12 +1,43 @@
+//print a #
 //console.log("#");
 
+//print 5 #s
 // for (let i = 0; i <= 4; i++) {
 //     console.log("#");
 // }
 
+//print 5 #s, in 5 lines
 // for (let row = 1; row <= 5; row++) {
 //     for (let i = 0; i <= 4; i++) {
 //         console.log("#");
 //     } console.log(""); 
 // }
 
+//print #s equal to row number, for all 5 rows, in reverse order
+// for (let row = 1; row <= 5; row++) {
+//     for (let i = 0; i < 6-row; i++) {
+//         console.log("#");
+//     } console.log("");
+// }
+
+//print #s going from 4 to 1, and then back to 4.
+// for (let row = 1; row <= 7; row++) {
+//     for (let i = 0; i < Math.abs(row-4); i++) {
+//         console.log("#");
+//     } console.log("");
+// }
+
+//print #s going from 1 to 4 and then back to 1.
+// for (let row = 1; row <= 7; row++) {
+//     for (let i = 0; i < (4-(Math.abs(row - 4))); i++) {
+//         console.log("#");
+//     } console.log("");
+// }
+
+//same as above, practice
+// for(row = 1; row <= 7; row++) {
+//     for(i = 0; i < 4 - Math.abs(4 - row); i++) {
+//         console.log("#");
+//     }
+//         console.log("");
+// }
