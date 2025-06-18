41aaad9cda6bd8ca345fb4b56f3b5953c4dd76e8
prthivy <prithviisinamood@gmail.com>
2025-06-18 11:31:30 +0530

    add comments

--- ch-02/input-processing/luhn-checksum-c.js
+++ ch-02/input-processing/luhn-checksum-c.js
@@ -1,12 +1,13 @@
-//goal: handle doubling for odd/even and >= 10 cases
+//goal: handle deciding digit for doubling, for odd & even nos. 
 
 //423765 -> '2', '7', '5' will be doubled, or doubler will be called for those values
 //pos 0 to len (= 6)
 
 
-
+//this function has a counter for the entire length of string,
+//using % operator it decides if the value is to be doubled or not before adding
 function evenCheckSum(num) {
-    for (let pos = 0; pos < num.length; pos++)
+    for (let pos = 0; pos < num.length; pos++) 
         if (pos % 2 == 1) {
             checkSum += doubler(num.slice(pos, pos + 1))
         }
