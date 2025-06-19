371951ac6356e764eadefe723ede910031662d6e
prthivy <prithviisinamood@gmail.com>
2025-06-19 21:51:47 +0530

    develop solution logic

--- ch-02/tracking-state/decoding-messages-a.js
+++ ch-02/tracking-state/decoding-messages-a.js
@@ -5,30 +5,38 @@ let pos = 0;
 
 stream = prompt("enter coded message") //17, 324, 8605, 99999
 
-while (stream.slice(pos, pos + 1) !== ',' && stream.slice(pos, pos+1) !== "") {
-    encodedDigits +=  stream.slice(pos, pos+1); 
-    pos++; //
+//while (stream.charAt(pos, pos + 1) !== "" && stream.charAt(pos + 1, pos + 2) !== "") {
+while (pos < stream.length) { //tysm claude <3
+while (stream.charAt(pos) !== ',' && stream.charAt(pos) !== "") {
+    encodedDigits +=  stream.slice(pos, pos + 1); 
+    pos++; 
 } 
 
 console.log(encodedDigits); //t1
+encodedDigits = ""
 pos = pos + 2; 
 
+}
 
-
-while (stream.slice(pos, pos+1) !== ',' && stream.slice(pos, pos+1) !== "") {
-    encodedDigits +=  stream.slice(pos, pos+1); 
-    pos++; //
-} 
-
-console.log(encodedDigits); 
-
-//tracex 1: 
+//tracex 1: 17, 256
+//stream.length = 
 //pos = 0
-//TRUE, slice(0,1)
+//TRUE, charAt(0)
 //"" + "1"
 //pos = 1
-//TRUE, slice(1,2)
+//TRUE, charAt(1)
 //"1" + "7"
 //pos = 2
 //FALSE. 
+//decoding logic
+//pos = 4
+//TRUE, charAt(4)
+//"" + "2"
+//pos = 5
+//TRUE, charAt(5)
+//"2" + "5"
+//TRUE, charAt(6)
+//"25" + "6"
+//FALSE
+//pos = 8 
     
