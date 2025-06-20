890d0062e199b8e1d2c58ab8eaed9c8b8077e09b
prthivy <prithviisinamood@gmail.com>
2025-06-20 11:58:57 +0530

    develop solution logic

---
+++ ch-02/tracking-state/decoding-messages-b2.js
@@ -0,0 +1,10 @@
+//goal: convert char code to char
+
+let letter = 'R';
+letter.charCodeAt(0);
+
+let encoded = '83';
+String.fromCharCode(encoded);
+
+encodedDigits = 18;
+String.fromCharCode((encodedDigits % 27) + 64); 
