2a7544d53e381bed72ba349d9b616f160a3948e4
prthivy <prithviisinamood@gmail.com>
2025-06-20 12:22:55 +0530

    develop solution logic

--- ch-02/tracking-state/decoding-messages-b.js
+++ ch-02/tracking-state/decoding-messages-b.js
@@ -23,30 +23,31 @@ while (stream.charAt(pos) !== ',' && stream.charAt(pos) !== "") {
 
 //when overall i/p ends, print the decoded output. 
 
-if (mode = 'UPPERCASE' && encodedDigits % 27 !== 0) {
+if (mode === 'UPPERCASE' && encodedDigits % 27 !== 0) {
     decodedLetter += String.fromCharCode((encodedDigits % 27) + 64);  
 }
 
 else { 
-mode = 'LOWERCASE';  
+    mode = 'LOWERCASE';  
 }
 
-if (mode = 'LOWERCASE' && encodedDigits % 27 !== 0) {
+if (mode === 'LOWERCASE' && encodedDigits % 27 !== 0) {
     decodedLetter += String.fromCharCode((encodedDigits % 27) + 96);  
 }
 
 else { 
-mode = 'PUNCTUATION'; 
+    mode = 'PUNCTUATION';
 }
 
-if (mode = 'LOWERCASE' && encodedDigits % 27 !== 0) {
-    decodedLetter += String.fromCharCode((encodedDigits % 27) + 96);  
+if (mode === 'PUNCTUATION' && encodedDigits % 27 !== 0) {
+    //symbol table logic 
 }
 
 else { 
-mode = 'UPPERCASE'; 
+    mode = 'UPPERCASE';
 }
 
+console.log(decodedLetter);
 
 
 console.log(encodedDigits);
