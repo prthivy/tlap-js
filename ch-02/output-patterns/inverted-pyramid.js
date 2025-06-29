d5ecd15729c485a603df849a0d342dd8724c5f4e
prthivy <prithviisinamood@gmail.com>
2025-06-29 13:17:19 +0530

    clean comments

---
+++ ch-02/output-patterns/inverted-pyramid.js
@@ -0,0 +1,15 @@
+for(row = 1; row < 4; row++) {
+    pattern = "";
+    for(i = 0; i < row - 1; i++) {
+        pattern += " ";
+    }
+    for(j = 0; j < 3 - (row - 1); j ++) {
+        pattern += "#";
+    }
+    console.log(pattern);
+}
+
+//what i want(from core equation)
+//space: row - 1
+//sym: 3 - space = (3 - (row - 1))
+
