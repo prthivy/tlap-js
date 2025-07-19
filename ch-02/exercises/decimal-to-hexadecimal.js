fbeb955ca6c5685eca362f3c748828fd83c71629
prthivy <prithviisinamood@gmail.com>
2025-07-19 18:30:28 +0530

    develop solution logic

--- ch-02/exercises/decimal-to-hexadecimal.js
+++ ch-02/exercises/decimal-to-hexadecimal.js
@@ -1,4 +1,53 @@
 let hexadecimal = "";
 let decimal = "";
+let remainder = "";
+let exponent = 31;
+let intQuotient = "";
 
-decimal = prompt("enter decimal");
+decimal = prompt("enter decimal")
+console.log(decimal);
+
+remainder = decimal;
+
+while(remainder !== 0) {
+    if(remainder - Math.pow(16, exponent) >= 0) {
+        intQuotient = Math.floor(remainder/Math.pow(16, exponent));
+        remainder -= (Math.pow(16, exponent) * intQuotient);
+        console.log(remainder);
+        if(intQuotient < 10) {
+            hexadecimal += intQuotient;
+        }
+        else {
+            switch (intQuotient) {
+            case 10:
+                hexadecimal += "A";
+                break;
+            case 11:
+                hexadecimal += "B";
+                break;
+            case 12: 
+                hexadecimal += "C";
+                break;
+            case 13: 
+                hexadecimal += "D";
+                break;
+            case 14:
+                hexadecimal += "E";
+                break;
+            case 15: 
+                hexadecimal += "F";
+                break;
+            default:
+                break;
+        }
+        exponent--;
+    }
+}
+
+    else {
+        exponent--;
+    }
+
+}
+
+console.log(hexadecimal);
--- ch-02/exercises/textbin.md
+++ ch-02/exercises/textbin.md
@@ -1,4 +1,4 @@
-## number-conversion-a
+## number-conversion-decimal-to-binary
 
 tracex:
 120
@@ -50,7 +50,7 @@ rem = 1
 if(1 - 1 < 1) true
 binary = 
 
-## number conversion-a2
+## number conversion-binary-to-decimal
 
 1011010
 pos 1 -> pos 7
@@ -98,3 +98,35 @@ pos 7
 decimal = 90
 decimal = 90 + 0 = 90
 
+## number conversion-decimal-to-hexadecimal
+
+tracex:
+
+test 1: "25290"
+
+25290 - (16, 3) >= 0
+25290 - (16, 3) * floor(25920/pow(16, 3))
+iQuotient = "6"
+25290 - (4096 * 6) = 714
+
+714 - (16, 2) >= 0
+714 - (16, 2) * floor(714/pow(16, 2))
+iQuotient = "2"
+714 - (256 * 2) = 202
+
+202 - (16, 1) >= 0
+202 - (16, 1) * floor(202/pow(16, 1))
+iQuotient = 12 ("C")
+202 - (16 * 12) = 10
+
+10 - (16, 0) >= 0
+10 - (16, 0) * floor(10/pow(16, 0))
+iQuotient = 10 ("A")
+10 - (1 * 10) = 0
+
+
+test 2: "69420"
+
+69420 - (16, 3) >= 0
+69420 - (16, 3) * floor(69420/pow(16, 3))
+iQuotient = 16 (??)
