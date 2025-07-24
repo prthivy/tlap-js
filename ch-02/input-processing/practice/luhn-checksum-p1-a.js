053088eef0aead17885afac0a32aa457c541f677
prthivy <prithviisinamood@gmail.com>
2025-07-24 22:36:32 +0530

    finalize solution and documentation

--- ch-02/input-processing/practice/luhn-checksum-p1-a.js
+++ ch-02/input-processing/practice/luhn-checksum-p1-a.js
@@ -1,12 +1,53 @@
 //browser
-let digit;
-let num = "";
+checkSum = 0; 
+evenCheckSum = 0;
+oddCheckSum = 0;
+let pos = 1;
 
-while(digit !== "" && digit !== null) {
+function doubler(doubled) {
+    doubled *= 2;
+    if (doubled > 9) {
+        doubled = 1 + doubled % 10;
+        return doubled;   
+    }
+    else {
+        return doubled;
+    }
+}
+
+let digit = prompt("Enter digit: ");
+
+while(digit !== "" && digit !== null) { 
+    if(pos % 2 === 1) {
+        evenCheckSum += doubler(digit);
+        oddCheckSum += +digit;
+
+        console.log(evenCheckSum);
+        console.log(oddCheckSum);
+    }
+    else {
+        evenCheckSum += +digit; 
+        oddCheckSum += doubler(digit);
+
+        console.log(evenCheckSum);
+        console.log(oddCheckSum);
+    }
     digit = prompt("Enter digit: ");
-    num += digit; 
+    pos++; 
 } 
-console.log(num); 
+console.log(evenCheckSum);
 
+if((pos - 1) % 2 === 0) {
+    checkSum = evenCheckSum;
+}
 
+else {
+    checkSum = oddCheckSum;
+}
 
+if(checkSum % 10 === 0) {
+    console.log("valid!");
+}
+else {
+    console.log('invalid..');
+}
---
+++ ch-02/input-processing/practice/textbin-lc-p1.md
@@ -0,0 +1,21 @@
+## What I've learnt:
+- Understanding return statements is key
+  - especially that they terminate a loop
+- Array index whoopsies:
+  - i = 0 to i = (length - 1)
+- String vs Number whoopsies
+- Lack of console.log in testing:
+  - use console.log liberally to debug
+- Major error in input while loop:
+  - **When collecting input until a sentinel, collect first input outside the while loop.**
+- Divide and conquer remains goated
+- And most importantly, understanding the problem. 
+
+tracex: 
+pos = 1
+pos 1, 1 pos = 2
+pos 2, 2, pos = 3
+pos 3, 3, pos = 4
+pos 4, 4 pos = 5
+pos 5, 4, pos = 6
+pos 6, 2, pos = 7
